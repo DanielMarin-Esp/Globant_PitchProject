33c0edbe6e954cd0b01046b2674e05163e72b073
Daniel <marin.esp.daniel@gmail.com>
2021-09-21 07:23:04 -0500

    Final Commit

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+
 window.addEventListener("scroll", function() {
     var banner = document.getElementById("navBar");
 
     banner.classList.toggle("sticky", window.scrollY > 0);
 })
+
+
+const navSlide = () => {
+    const menu = document.getElementsByClassName("menu");
+    
+    menu.addEventListener('click', function() {
+        var nav = document.getElementsByClassName("nav_links");
+
+        nav.classList.toggle("nav_active");
+    })
+}
+
+const navDrop = () => {
+
+}
+
